b6de87239a29f7013eb9227019775dabd370dfe6
David Perez <d.perez89@aol.com>
2017-11-27 10:35:15 -0600

    adding in the necessary classes to the code.

---
+++ LemonadeStand/LemonadeStand/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Game
+    {
+        Player player1;
+        Store store;
+        List<Customer> customers;
+        Day day;
+
+        public Game()
+        {
+
+        }
+
+        //methods
+    }
+}
